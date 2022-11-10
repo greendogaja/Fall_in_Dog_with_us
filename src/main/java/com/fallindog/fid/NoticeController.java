f755df5be04c3ecdb8afc02dd7a247790e36cc5a
serapark516 <bvely718@gmail.com>
2022-11-10 22:36:35 +0900

    test

--- src/main/java/com/fallindog/fid/NoticeController.java
+++ src/main/java/com/fallindog/fid/NoticeController.java
@@ -20,7 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import service.NoticeService;
 import vo.NoticeVO;
 
-//test
+//testtesttesttest
 @Controller 
 public class NoticeController {
   
