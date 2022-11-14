eda57c426a87677bb287122f5d2d7ef4f2ff594c
serapark516 <bvely718@gmail.com>
2022-11-14 18:47:42 +0900

    Revert "Revert "request.getSession().setAttribute("loginImg", vo.getUploadfile());""
    This reverts commit 4be7274e2c672b62150270526d5199a59fb037f2.

--- src/main/java/com/fallindog/fid/UserController.java
+++ src/main/java/com/fallindog/fid/UserController.java
@@ -142,6 +142,7 @@ public class UserController {
 				request.getSession().setAttribute("loginNick", vo.getNname());
 				request.getSession().setAttribute("loginName", vo.getName());
 				request.getSession().setAttribute("loginPW", password);
+				request.getSession().setAttribute("loginImg", vo.getUploadfile());
 				
 				Cookie cook = new Cookie("userId",vo.getId());
 				if (idsave != null && idsave.equals("cook")) {
--- src/main/java/vo/NoticeVO.java
+++ src/main/java/vo/NoticeVO.java
@@ -1,5 +1,7 @@
 package vo;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -12,5 +14,6 @@ public class NoticeVO {
 	private int cnt;
 	private String subject;
 	private String img;
+	private MultipartFile uploadfilef;
 	
 }
